f200fcd37da087bdc9347ec7a5160a72c3a6908f
pimtegelaar <pimtegelaar@gmail.com>
2015-02-08 12:26:53 +0100

    initial commit

---
+++ jenkinstest/src/test/java/jenkinstest/MainTest.java
@@ -0,0 +1,14 @@
+package jenkinstest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		Main.main(new String[]{});
+	}
+
+}
